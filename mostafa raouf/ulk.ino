278bbe63320b3a03ffce1e286a85a9ab92533154
mostafa roauf <36244885+mostafaabdo757585@users.noreply.github.com>
2018-02-19 09:14:42 -0800

    Add files via upload

---
+++ mostafa raouf/ulk.ino
@@ -0,0 +1,33 @@
+float value;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(7,HIGH);
+delay(1000);
+ digitalWrite(7,LOW);
+delay(1000);
+
+ digitalWrite(6,HIGH);
+delay(1000);
+ digitalWrite(6,LOW);
+delay(1000);
+
+ digitalWrite(5,HIGH);
+delay(1000);
+ digitalWrite(5,LOW);
+delay(1000);
+
+ digitalWrite(4,HIGH);
+delay(1000);
+ digitalWrite(4,LOW);
+delay(1000);
+}
