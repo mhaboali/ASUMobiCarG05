823a4110a1b9f35519f835dac8a18b4ec93c68d1
mostafaezz97 <36308269+mostafaezz97@users.noreply.github.com>
2018-02-20 05:37:06 +0200

    second week

---
+++ bouncing.ino
@@ -0,0 +1,15 @@
+
+void setup() {
+    // attaches the servo on pin 9 to the servo object}
+}
+void loop() {
+ int i ; 
+ digitalWrite ( 5 , LOW);
+ delay (600);
+ digitalWrite ( 5 , HIGH);
+ delay (200);
+  
+  
+  }
+
+
