62d3d2f38e55d9784728e4e1b4981a0098633b8c
mhmoudibrahim <36413349+mhmoudibrahim@users.noreply.github.com>
2018-02-21 16:25:55 +0200

    Add files via upload

---
+++ mahmoud ibrahim/DUT.C.ino
@@ -0,0 +1,16 @@
+int H=9;
+void setup() {
+  pinMode(H,OUTPUT);
+  // put your setup code here, to run once:
+}
+void loop()
+{
+   digitalWrite(H,HIGH); 
+   delay(500);  //to make duty.C=25%
+     digitalWrite(H,LOW); 
+      delay(1500);
+}
+  
+
+
+
