7b4e6ebfc25c452b0a0ee883afcaa9def66c391d
mohamadawaad <36459844+mohamadawaad@users.noreply.github.com>
2018-02-22 13:00:48 +0200

    Add files via upload

---
+++ mohmed awaad/sketch_feb18a.ino
@@ -0,0 +1,26 @@
+const int LED =13;
+const int b=2;
+int val ;
+void setup() {
+
+  pinMode(LED,OUTPUT);
+pinMode(b,INPUT)
+;}
+
+void loop() {
+  val=digitalRead(b);
+
+  if(val==HIGH){
+    
+    
+  digitalWrite(LED,HIGH);
+  delay(1000);
+
+    digitalWrite(LED,LOW);
+  delay(1000);
+  }
+  else{
+digitalWrite(LED,LOW);}
+    
+    }
+
